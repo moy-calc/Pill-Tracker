8adae9e0d69774d58d4b8ca36239bc9206dcc47d
Dental-guide <benosmanzeriab@gmail.com>
2026-01-07 00:41:23 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,24 @@
 const CACHE_NAME = 'medtracker-v1';
 const ASSETS = [
-  'Index.html',
-  'manifest.json',
-  'https://cdn.tailwindcss.com'
+  './',
+  './Index.html',
+  './manifest.json',
+  './icon.png'
 ];
 
-// Installation et mise en cache
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+self.addEventListener('install', e => {
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
 });
 
-// Intercepter les requêtes pour le mode hors-ligne
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+self.addEventListener('activate', e => {
+    e.waitUntil(self.clients.claim());
 });
 
-// Gestion des notifications push
-self.addEventListener('push', (e) => {
-  const data = e.data ? e.data.json() : { title: 'Rappel Médicament', body: 'Il est l\'heure de votre prise.' };
-  e.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: 'icon.png',
-      badge: 'icon.png'
-    })
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
 });
